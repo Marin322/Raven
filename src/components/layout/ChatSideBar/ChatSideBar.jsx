a8f8d29372ca7ade1283101a1db78e8f17927708
Marin322 <santik.987@mail.ru>
2026-01-02 16:42:29 +0500

    Доделан компонент ChatSideBar

--- src/components/layout/ChatSideBar/ChatSideBar.jsx
+++ src/components/layout/ChatSideBar/ChatSideBar.jsx
@@ -1,30 +1,35 @@
-import { useEffect, useState } from 'react';
-import styles from '../../../styles/components/layout/ChatSideBar/ChatSideBar.module.css';
+import { useEffect, useState } from "react";
+import styles from "../../../styles/components/layout/ChatSideBar/ChatSideBar.module.css";
 
 const ChatSideBar = ({ chatId }) => {
-    const [chatInfo, setChatInfo] = useState(null);
+  const [chatInfo, setChatInfo] = useState(null);
 
-    useEffect(() => {
-        GetChatInfo();
-        console.log(chatInfo)
-    }, []);
+  useEffect(() => {
+    GetChatInfo();
+    console.log(chatInfo);
+  }, []);
 
-    const GetChatInfo = async () => {
-        const { chatsService } = await import('../../../services/api/ChatsService');
-        try {
-            const chatInfo = await chatsService.GetChatInfo(chatId)
-            setChatInfo(chatInfo);
-        }
-        catch {}
-    }
+  const GetChatInfo = async () => {
+    const { chatsService } = await import("../../../services/api/ChatsService");
+    try {
+      const chatInfo = await chatsService.GetChatInfo(chatId);
+      setChatInfo(chatInfo);
+    } catch {}
+  };
 
-    return (
-        <div>
-            {chatInfo && (
-                <div>{chatInfo.name}</div>
-            )}
+  return (
+    <div className={styles["chatSideBar-container"]}>
+      {chatInfo && (
+        <div className="w-full h-full flex items-center flex-col p-3">
+          <div className={styles["chatSideBar-avatar"]}>
+            <img />
+            <a>{chatInfo.name}</a>
+          </div>
+          <div className={styles["chatSideBar-desc"]}>{chatInfo.description}</div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ChatSideBar;
